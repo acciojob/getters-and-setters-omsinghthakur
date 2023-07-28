ca39f6c0866f52ab3cdb62c6150084b251a245c6
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-07-28 13:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 //complete this code
-class Person {}
+class Person {
+    private String name;
+    private int age;
 
-class Student extends Person {}
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-class Teacher extends Person {}
+    public String getName() {
+        return name;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public void study() {
+        System.out.println(getName() + " is studying");
+    }
+}
+
+class Teacher extends Person {
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public void teach() {
+        System.out.println(getName() + " is teaching");
+    }
+	 }   
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
